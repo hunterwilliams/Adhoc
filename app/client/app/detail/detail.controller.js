535fd8d38eb495fac78d37baae0df19e7d7a4b1f
Hunter Williams <hunter@huntergwilliams.com>
2015-04-13 10:24:46 -0400

    Made the details page correctly display documents. Doesn't correctly get from endpoint yet though..

--- app/client/app/detail/detail.controller.js
+++ app/client/app/detail/detail.controller.js
@@ -4,7 +4,13 @@ angular.module('demoApp')
   .controller('DetailCtrl', function ($scope, $http) {
 
     // Use the User $resource to fetch all users
-    $scope.document = {permissions:[{'role':'test','method':'read'}],collections:['a','b'],document:'lots of text goes here'};
+    $scope.doc = {type:'Test',permissions:[{'role':'test','method':'read'}],collections:['a','b'],text:'lots of text goes here',
+      related:[
+        {
+          type:'Type A', items:[{uri:'abc.html',db:'Documents'}]
+        }
+      ]
+    };
     $scope.database = 'Test';//$routeParams.database;
     $scope.uri      = 'document.xml';//$routeParams.uri;
   });
